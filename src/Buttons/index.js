f607a6e66e02ca166133a44f18f98280d62205da
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-08 13:45:00 +0200

    tabs spaces changed to 2 from 4

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,20 +2,20 @@ import "./style.css"
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, toggleAllDone }) => (
 
-	<span className="buttons">
-		{tasks.length > 0 && (
-			<>
-				<button onClick={toggleHideDone} className="buttons__button">
-					{hideDone ? "Pokaż" : "Ukryj"} ukończone
-				</button>
-				<button
-					className="buttons__button"
-					onClick={toggleAllDone}
-					disabled={tasks.every(({ done }) => done)}>
-					Ukończ wszystkie
-				</button>
-			</>)}
-	</span>
+  <span className="buttons">
+    {tasks.length > 0 && (
+      <>
+        <button onClick={toggleHideDone} className="buttons__button">
+          {hideDone ? "Pokaż" : "Ukryj"} ukończone
+        </button>
+        <button
+          className="buttons__button"
+          onClick={toggleAllDone}
+          disabled={tasks.every(({ done }) => done)}>
+          Ukończ wszystkie
+        </button>
+      </>)}
+  </span>
 );
 
 export default Buttons;
--- src/Form/index.js
+++ src/Form/index.js
@@ -2,24 +2,26 @@ import { useState } from 'react';
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
-    const [newTaskContent, setNewTaskContent] = useState("");
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        setNewTaskContent("");
-        if (newTaskContent !== "") { addNewTask(newTaskContent.trim()) }
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setNewTaskContent("");
+    if (newTaskContent.trim() === "") { return }
+    addNewTask(newTaskContent.trim())
 
-    return (
-        <form onSubmit={onFormSubmit} className="form">
-            <input value={newTaskContent}
-                className="form__newTask"
-                placeholder="Co jest do zrobienia"
-                onChange={({ target }) => setNewTaskContent(target.value)}
-            />
-            <button className="form__addTaskButton">Dodaj zadanie</button>
-        </form>
-    );
+  };
+
+  return (
+    <form onSubmit={onFormSubmit} className="form">
+      <input value={newTaskContent}
+        className="form__newTask"
+        placeholder="Co jest do zrobienia"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <button className="form__addTaskButton">Dodaj zadanie</button>
+    </form>
+  );
 }
 
 export default Form;
