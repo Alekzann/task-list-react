1452f4c0eabf52df45716ce96ca27938e2b2d187
Oleksandr Riaboshapka <hadion@mail.ru>
2024-01-25 00:14:27 +0100

    Added router navigation to the project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,41 @@
-import Tasks from "./features/tasks/Tasks.js";
+import TasksPage from "./features/tasks/TasksPage/index.js";
+import TaskPage from "./features/tasks/TaskPage/index.js";
+import AuthorPage from "./features/author/AuthorPage.js";
+import {
+  HashRouter,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+} from "react-router-dom/cjs/react-router-dom.min.js";
 
-export default () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/zadania">Zadania</NavLink >
+        </li>
+        <li>
+          <NavLink to="/author">Author</NavLink >
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/author">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
+
+export default App;
